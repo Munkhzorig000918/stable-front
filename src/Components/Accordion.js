fcc97bc6091fb46e56a4e7d49101fd6027b2f1aa
Tuvshinbayar <tuvshinbayar.dev@gmail.com>
2021-10-27 16:50:01 +0800

    Changed FavIcon

--- src/Components/Accordion.js
+++ src/Components/Accordion.js
@@ -12,40 +12,30 @@ const Accordion = () => {
                 </div>
                 <div className="m-8 rounded overflow-hidden">
                     <div className="group outline-none accordion-section" tabindex="1">
-                        <div className="group flex justify-between px-4 py-3 items-center text-gray-600 transition ease duration-500 cursor-pointer pr-10 relative">
-                        <div className="group-focus:text-gray-600 transition ease duration-500">
-                            Стэйблкойн гэж юу вэ? 
+                        <div className="group flex justify-between px-4 py-3 items-center text-gray-600 transition ease duration-500 cursor-pointer pr-10 ">
+                            <div className="group-focus:text-gray-600 transition ease duration-500">
+                                Стэйблкойн гэж юу вэ? 
+                            </div>
+                            <div className="h-8 w-8 border border-gray-700 rounded-full items-center inline-flex justify-center transform transition ease duration-500 group-focus:text-gray-600 group-focus:-rotate-180 absolute top-0 right-0 mb-auto ml-auto mt-2 mr-2">
+                                <FaAngleDown />
+                            </div>
                         </div>
-                        <div
-                            className="h-8 w-8 border border-gray-700 rounded-full items-center inline-flex justify-center transform transition ease duration-500 group-focus:text-gray-600 group-focus:-rotate-180 absolute top-0 right-0 mb-auto ml-auto mt-2 mr-2"
-                            >
-                            <FaAngleDown />
-                        </div>
-                        </div>
-                        <div
-                        className="group-focus:max-h-screen max-h-0  px-4 overflow-hidden ease duration-500"
-                        >
+                        <div className="group-focus:max-h-screen max-h-0  px-4 overflow-hidden ease duration-500">
                         <p className="p-2 text-gray-700 text-justify">
                             Стэйблкойн нь валютын нөөцөөр баталгаажиж, үнэ цэнээ аргамжсан криптовалютын төрөл юм.
                         </p>
                         </div>
                     </div>
                     <div className="group outline-none accordion-section" tabindex="2">
-                        <div
-                        className="group  flex justify-between px-4 py-3 items-center text-gray-600 transition ease duration-500 cursor-pointer pr-10 relative"
-                        >
+                        <div className="group  flex justify-between px-4 py-3 items-center text-gray-600 transition ease duration-500 cursor-pointer pr-10 ">
                         <div className="group-focus:text-gray-600 transition ease duration-500">
                             MONT гэж юу вэ?
                         </div>
-                        <div
-                            className="h-8 w-8 border border-gray-700 rounded-full items-center inline-flex justify-center transform transition ease duration-500 group-focus:text-gray-600 group-focus:-rotate-180 absolute top-0 right-0 mb-auto ml-auto mt-2 mr-2"
-                            >
+                        <div className="h-8 w-8 border border-gray-700 rounded-full items-center inline-flex justify-center transform transition ease duration-500 group-focus:text-gray-600 group-focus:-rotate-180 absolute top-0 right-0 mb-auto ml-auto mt-2 mr-2" >
                             <FaAngleDown />
                         </div>
                         </div>
-                        <div
-                        className="group-focus:max-h-screen max-h-0 px-4 overflow-hidden ease duration-500"
-                        >
+                        <div className="group-focus:max-h-screen max-h-0 px-4 overflow-hidden ease duration-500">
                         <p className="p-2 text-gray-700 text-justify">
                             MONT нь Монгол төгрөгт аргамжсан стэйблкойн юм.
                         </p>
@@ -53,7 +43,7 @@ const Accordion = () => {
                     </div>
                     <div className="group outline-none accordion-section" tabindex="3">
                         <div
-                        className="group flex justify-between px-4 py-3 items-center text-gray-600 transition ease duration-500 cursor-pointer pr-10 relative"
+                        className="group flex justify-between px-4 py-3 items-center text-gray-600 transition ease duration-500 cursor-pointer pr-10 "
                         >
                         <div className="group-focus:text-gray-600 transition ease duration-500">
                             MONT нь аль блокчэйн дээр нэвтэрсэн байгаа вэ?
@@ -74,7 +64,7 @@ const Accordion = () => {
                     </div>
                     <div className="group outline-none accordion-section" tabindex="3">
                         <div
-                        className="group flex justify-between px-4 py-3 items-center text-gray-600 transition ease duration-500 cursor-pointer pr-10 relative"
+                        className="group flex justify-between px-4 py-3 items-center text-gray-600 transition ease duration-500 cursor-pointer pr-10 "
                         >
                         <div className="group-focus:text-gray-600 transition ease duration-500">
                             MONT нь ямар үнэ цэнтэй бэ?
@@ -95,7 +85,7 @@ const Accordion = () => {
                     </div>
                     <div className="group outline-none accordion-section" tabindex="3">
                         <div
-                        className="group flex justify-between px-4 py-3 items-center text-gray-600 transition ease duration-500 cursor-pointer pr-10 relative"
+                        className="group flex justify-between px-4 py-3 items-center text-gray-600 transition ease duration-500 cursor-pointer pr-10 "
                         >
                         <div className="group-focus:text-gray-600 transition ease duration-500">
                             Хэрхэн MONT худалдаж авах вэ?
@@ -116,7 +106,7 @@ const Accordion = () => {
                     </div>
                     <div className="group outline-none accordion-section" tabindex="3">
                         <div
-                        className="group flex justify-between px-4 py-3 items-center text-gray-600 transition ease duration-500 cursor-pointer pr-10 relative"
+                        className="group flex justify-between px-4 py-3 items-center text-gray-600 transition ease duration-500 cursor-pointer pr-10 "
                         >
                         <div className="group-focus:text-gray-600 transition ease duration-500">
                             Хэрхэн MONT-ыг шилжүүлэх вэ?
