ce519238336f2f5a981d5a5d2bff52d497b1de4a
Tuvshinbayar <tuvshinbayar.dev@gmail.com>
2021-10-29 03:36:42 +0800

    added accordion

--- src/Components/Accordion.js
+++ src/Components/Accordion.js
@@ -2,143 +2,134 @@ import React from "react";
 import './accordion.css'
 import { FaAngleDown } from "react-icons/fa";
 import Bubble from '../assets/Frame.png'
+import {Disclosure, Transition} from '@headlessui/react';
+import {BsChevronDown, BsChevronUp} from 'react-icons/bs';
 
 const Accordion = () => {
     return(
-        <div>
-            <div className="container mx-auto py-28" style={{backgroundImage: `url(${Bubble})`}}>
-                <div className="">
-                    <h1 id="faq" className="text-center font-bold text-4xl">Түгээмэл асуулт хариулт</h1>
-                </div>
-                <div className="m-8 rounded overflow-hidden">
-                    <div className="group outline-none accordion-section" tabindex="1">
-                        <div className="group flex justify-between px-4 py-3 items-center text-gray-600 transition ease duration-500 cursor-pointer pr-10">
-                        <div className="group-focus:text-gray-600 transition ease duration-500  font-extrabold lg:text-2xl">
-                            Стэйблкойн гэж юу вэ?
-                        </div>
-                        <div className="">
-                            <FaAngleDown />
-                        </div>
-                        </div>
-                        <div className="group-focus:max-h-screen max-h-0  px-4 overflow-hidden ease duration-500">
-                        <p className="p-2 text-gray-700 text-justify">
-                            Стэйблкойн нь валютын нөөцөөр баталгаажиж, үнэ цэнээ аргамжсан криптовалютын төрөл юм.
-                        </p>
-                        </div>
-                    </div>
-                    <div className="group outline-none accordion-section" tabindex="2">
-                        <div className="group  flex justify-between px-4 py-3 items-center text-gray-600 transition ease duration-500 cursor-pointer pr-10">
-                        <div className="group-focus:text-gray-600 transition ease duration-500 font-extrabold lg:text-2xl ">
-                            MONT гэж юу вэ?
-                        </div>
-                        <div className="">
-                            <FaAngleDown />
-                        </div>
-                        </div>
-                        <div className="group-focus:max-h-screen max-h-0 px-4 overflow-hidden ease duration-500">
-                        <p className="p-2 text-gray-700 text-justify">
-                            MONT нь Монгол төгрөгт аргамжсан стэйблкойн юм.
-                        </p>
-                        </div>
-                    </div>
-                    <div className="group outline-none accordion-section" tabindex="3">
-                        <div
-                        className="group flex justify-between px-4 py-3 items-center text-gray-600 transition ease duration-500 cursor-pointer pr-10"
-                        >
-                        <div className="group-focus:text-gray-600 transition ease duration-500 font-extrabold lg:text-2xl">
-                            MONT нь аль блокчэйн дээр нэвтэрсэн байгаа вэ?
-                        </div>
-                        <div className="">
-                            <FaAngleDown />
-                        </div>
-                        </div>
-                        <div
-                        className="group-focus:max-h-screen max-h-0 px-4 overflow-hidden ease duration-500"
-                        >
-                        <p className="p-2 text-gray-700 text-justify">
-                            MONT нь Binance Smart Chain болон Ethereum блокчэйн сүлжээнд байршсан бөгөөд удалгүй Solana, Cardano зэрэг блокчэйнд нэвтрэх ажил хийгдэж байна.
-                        </p>
-                        </div>
-                    </div>
-                    <div className="group outline-none accordion-section" tabindex="3">
-                        <div
-                        className="group flex justify-between px-4 py-3 items-center text-gray-600 transition ease duration-500 cursor-pointer pr-10"
-                        >
-                        <div className="group-focus:text-gray-600 transition ease duration-500 font-extrabold lg:text-2xl">
-                            MONT нь ямар үнэ цэнтэй бэ?
-                        </div>
-                        <div className="">
-                            <FaAngleDown />
-                        </div>
-                        </div>
-                        <div
-                        className="group-focus:max-h-screen max-h-0 px-4 overflow-hidden ease duration-500"
-                        >
-                        <p className="p-2 text-gray-700 text-justify">
-                            MONT нь ямагт Монгол төгрөгтэй 1:1 харьцах үнэ цэнтэй байх болно.
-                        </p>
-                        </div>
-                    </div>
-                    <div className="group outline-none accordion-section" tabindex="3">
-                        <div
-                        className="group flex justify-between px-4 py-3 items-center text-gray-600 transition ease duration-500 cursor-pointer pr-10"
-                        >
-                        <div className="group-focus:text-gray-600 transition ease duration-500 font-extrabold lg:text-2xl">
-                            Хэрхэн MONT худалдаж авах вэ?
-                        </div>
-                        <div className="">
-                            <FaAngleDown />
-                        </div>
-                        </div>
-                        <div
-                        className="group-focus:max-h-screen max-h-0 px-4 overflow-hidden ease duration-500"
-                        >
-                        <p className="p-2 text-gray-700 text-justify">
-                            Та MONT-ыг өөрийн хэрэглээний виртуал хөрөнгийн бирж дээр арилжиж авах боломжтой. Бид MONT-ыг дотоодын болон олон улсын биржүүдэд нэвтрүүлэхээр ажиллаж байна.
-                        </p>
-                        </div>
-                    </div>
-                    <div className="group outline-none accordion-section" tabindex="3">
-                        <div
-                        className="group flex justify-between px-4 py-3 items-center text-gray-600 transition ease duration-500 cursor-pointer pr-10"
-                        >
-                        <div className="group-focus:text-gray-600 transition ease duration-500 font-extrabold lg:text-2xl">
-                            Хэрхэн MONT-ыг шилжүүлэх вэ?
-                        </div>
-                        <div className="">
-                            <FaAngleDown />
-                        </div>
-                        </div>
-                        <div
-                        className="group-focus:max-h-screen max-h-0 px-4 overflow-hidden ease duration-500"
-                        >
-                        <p className="p-2 text-gray-700 text-justify">
-                            Та MONT-ыг өөрийн хэрэглээний бирж эсвэл хэтэвчээр дамжуулан шилжүүлэх боломжтой. Та шилжүүлэг хийхээсээ өмнө зөв сүлжээгээр (BEP-20, ERC-20, г.м) хийж байгаагаа дахин нягтлах хэрэгтэй.
-                        </p>
-                        </div>
-                    </div>
-                    <div className="group outline-none accordion-section" tabindex="3">
-                        <div
-                        className="group flex justify-between px-4 py-3 items-center text-gray-600 transition ease duration-500 cursor-pointer pr-10"
-                        >
-                        <div className="group-focus:text-gray-600 transition ease duration-500 font-extrabold lg:text-2xl">
-                            Яагаад MONT гэж?
-                        </div>
-                        <div className="">
-                            <FaAngleDown />
-                        </div>
-                        </div>
-                        <div
-                        className="group-focus:max-h-screen max-h-0 px-4 overflow-hidden ease duration-500"
-                        >
-                        <p className="p-2 text-gray-700 text-justify">
-                            MONT нь Монгол иргэдийн хэрэглээнд хамгийн дөхөм, найдвартай бөгөөд аюулгүй бүтээгдсэн стэйблкойн юм. MONT-ын аюулгүй байдлыг Олон улсын блокчэйн аудитын компани болох Certik аудит хийж баталгаажуулсан болно.
-                        </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+    <div className="w-full px-4 pt-16 pb-16">
+      <div className="container p-2 mx-auto rounded-2xl">
+        <Disclosure>
+          {({ open }) => (
+            <>
+              <Disclosure.Button className="flex justify-between w-full p-5 text-sm font-medium text-left rounded-lg hover:bg-gray-400 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+                <span className="text-xl">Стэйблкойн гэж юу вэ?</span>
+                <BsChevronDown
+                  className={`${
+                    open ? 'transform rotate-180' : ''
+                  } w-5 h-5 text-black`}
+                />
+              </Disclosure.Button>
+              <Disclosure.Panel className="px-4 pt-4 pb-2 text-md text-gray-700">
+                Стэйблкойн нь валютын нөөцөөр баталгаажсан, үнэ цэнийг энгийн хэрэглээний валюттай аргамжсан криптовалютын төрөл юм.
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+        <Disclosure as="div" className="mt-2">
+          {({ open }) => (
+            <>
+              <Disclosure.Button className="flex justify-between w-full p-5 text-sm font-medium text-left rounded-lg hover:bg-gray-400 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+                <span className="text-xl">MONT гэж юу вэ?</span>
+                <BsChevronDown
+                  className={`${
+                    open ? 'transform rotate-180' : ''
+                  } w-5 h-5 text-black`}
+                />
+              </Disclosure.Button>
+              <Disclosure.Panel className="px-4 pt-4 pb-2 text-md text-gray-700">
+                MONT нь Монгол төгрөгт аргамжсан стэйблкойн юм.
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+        <Disclosure as="div" className="mt-2">
+          {({ open }) => (
+            <>
+              <Disclosure.Button className="flex justify-between w-full p-5 text-sm font-medium text-left rounded-lg hover:bg-gray-400 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+                <span className="text-xl">MONT нь аль блокчэйн платформдбайршсан бэ?</span>
+                <BsChevronDown
+                  className={`${
+                    open ? 'transform rotate-180' : ''
+                  } w-5 h-5 text-black`}
+                />
+              </Disclosure.Button>
+              <Disclosure.Panel className="px-4 pt-4 pb-2 text-md text-gray-700">
+                MONT нь Binance Smart Chain болон Ethereum блокчэйн сүлжээнд байршсан бөгөөд удалгүй Solana, Cardano зэрэг блокчэйнд нэвтрэх ажил хийгдэж байна.
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+        <Disclosure as="div" className="mt-2">
+          {({ open }) => (
+            <>
+              <Disclosure.Button className="flex justify-between w-full p-5 text-sm font-medium text-left rounded-lg hover:bg-gray-400 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+                <span className="text-xl">MONT нь ямар үнэ цэнтэй бэ?</span>
+                <BsChevronDown
+                  className={`${
+                    open ? 'transform rotate-180' : ''
+                  } w-5 h-5 text-black`}
+                />
+              </Disclosure.Button>
+              <Disclosure.Panel className="px-4 pt-4 pb-2 text-md text-gray-700">
+                MONT нь ямагт Монгол төгрөгтэй 1:1 харьцах үнэ цэнтэй байх болно.
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+        <Disclosure as="div" className="mt-2">
+          {({ open }) => (
+            <>
+              <Disclosure.Button className="flex justify-between w-full p-5 text-sm font-medium text-left rounded-lg hover:bg-gray-400 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+                <span className="text-xl">Хэрхэн MONT худалдаж авах вэ?</span>
+                <BsChevronDown
+                  className={`${
+                    open ? 'transform rotate-180' : ''
+                  } w-5 h-5 text-black`}
+                />
+              </Disclosure.Button>
+              <Disclosure.Panel className="px-4 pt-4 pb-2 text-md text-gray-700">
+                Та MONT-ыг өөрийн үйлчлүүлдэгвиртуал хөрөнгийн биржийн арилжаагааравах боломжтой. Бид MONT-ийг дотоод болон гадаадын олон биржид нэвтрүүлэхээр ажиллаж байна.
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+        <Disclosure as="div" className="mt-2">
+          {({ open }) => (
+            <>
+              <Disclosure.Button className="flex justify-between w-full p-5 text-sm font-medium text-left rounded-lg hover:bg-gray-400 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+                <span className="text-xl">Хэрхэн MONT-ийг шилжүүлэх вэ?</span>
+                <BsChevronDown
+                  className={`${
+                    open ? 'transform rotate-180' : ''
+                  } w-5 h-5 text-black`}
+                />
+              </Disclosure.Button>
+              <Disclosure.Panel className="px-4 pt-4 pb-2 text-md text-gray-700">
+                 Та MONT-ыг өөрийн үйлчлүүлдэгбирж эсвэл хэтэвчээр дамжуулан шилжүүлэх боломжтой. Та шилжүүлэг хийхээсээ өмнө зөв сүлжээг (BEP-20, ERC-20, г.м) ашиглажбайгаагаа дахин нягтлаарай.
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+        <Disclosure as="div" className="mt-2">
+          {({ open }) => (
+            <>
+              <Disclosure.Button className="flex justify-between w-full p-5 text-sm font-medium text-left rounded-lg hover:bg-gray-400 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+                <span className="text-xl">Яагаад MONT гэж?</span>
+                <BsChevronDown
+                  className={`${
+                    open ? 'transform rotate-180' : ''
+                  } w-5 h-5 text-black`}
+                />
+              </Disclosure.Button>
+              <Disclosure.Panel className="px-4 pt-4 pb-2 text-md text-gray-700">
+                MONT нь Монгол хүнхэрэглэхэдхамгийн дөхөм, найдвартай, аюулгүй стэйблкойн юм. MONT-ийн аюулгүй байдалд олон улсын блокчэйн аудитын компани болох Certik аудит хийн баталгаажуулсан болно.
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+      </div>
+    </div>
     )
 }
 
