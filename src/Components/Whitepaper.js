0df5efc832704f28811dcb2369b603c895673195
Tuvshinbayar <tuvshinbayar.dev@gmail.com>
2021-10-28 15:47:14 +0800

    added whitepaper

--- src/Components/Whitepaper.js
+++ src/Components/Whitepaper.js
@@ -9,7 +9,7 @@ const Whitepaper = () => {
                         {/* <p className="font-light text-gray-600 mt-5 lg:w-2/3">Крипто зах зээлд төгрөгийн үнэ цэнийг төлөөлөх хэрэгсэл болох MONT-ийн Техник, Технологи Эдийн Засгийн Үндэслэл (Whitepaper)-тэй дараах холбоосоор танилцаарай</p> */}
                     </div>
                     <div className="mt-20 lg:mt-0">
-                        <a className="p-10 bg-blue-500 hover:bg-blue-600 text-sm text-white font-bold rounded-xl transition duration-200" href="#">White paper</a>
+                        <a className="p-10 bg-blue-500 hover:bg-blue-600 text-sm text-white font-bold rounded-xl transition duration-200" target="_blank" href="https://drive.google.com/file/d/1TvWMfj1RFzFOiw6PYEIkuxMjCGmgBYaj/view?usp=sharing">White paper</a>
                     </div>
                 </div>
             </div>
