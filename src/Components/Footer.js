e21665ad1a566e0c84a1ce4050a48ee43c751415
Tuvshinbayar <tuvshinbayar.dev@gmail.com>
2021-10-28 23:46:53 +0800

    added links

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,9 +7,9 @@ import logo from "../assets/logo.png"
 const Footer = () => {
     return( 
         
-     <div className="bg-gray-900 text-white pt-12 pb-8 px-4">
+     <div id="contact" className="bg-gray-900 text-white pt-12 pb-8 px-4">
 	<div className="mx-auto px-4 container overflow-hidden flex flex-col lg:flex-row justify-between">
-		<a href="#" className="block mr-4 w-1/3"><img className="w-60" src={logo} /></a>
+		<a href="#" className="block mr-4 w-1/3"><img className="w-40" src={logo} /></a>
 		<div className="w-2/3 block sm:flex text-sm mt-6 lg:mt-0">
 			<ul className="text-gray-700 list-none p-0 font-thin flex flex-col text-left w-full">
 				<li className="inline-block py-2 px-3 text-white uppercase font-medium tracking-wide">Бүтээгдхүүн</li>
@@ -31,16 +31,16 @@ const Footer = () => {
 			<div className="text-gray-700 flex flex-col w-full">
 				<div className="inline-block py-2 px-3 text-white uppercase font-medium tracking-wide">Холбоо барих</div>
 				<div className="flex pl-4 justify-start mt-2">
-					<a className="block flex items-center text-gray-300 hover:text-white mr-6 no-underline" href="#">
+					<a className="block flex items-center text-gray-300 hover:text-white mr-6 no-underline" target="_blank" href="https://www.facebook.com/">
 						<FaFacebook />
 					</a>
-					<a className="block flex items-center text-gray-300 hover:text-white mr-6 no-underline" href="#">
+					<a className="block flex items-center text-gray-300 hover:text-white mr-6 no-underline" target="_blank" href="https://www.instagram.com/">
 						<FaInstagram />
 					</a>
-					<a className="block flex items-center text-gray-300 hover:text-white mr-6 no-underline" href="#">
+					<a className="block flex items-center text-gray-300 hover:text-white mr-6 no-underline" target="_blank" href="https://twitter.com/">
 						<FaTwitter />
 					</a>
-					<a className="block flex items-center text-gray-300 hover:text-white no-underline" href="#">
+					<a className="block flex items-center text-gray-300 hover:text-white no-underline" href="mailto:info@stablecoin.mn">
 						<FaMailBulk />
 					</a>
 				</div>
