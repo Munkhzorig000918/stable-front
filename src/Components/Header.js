6ac3e5db845400290ab5cd58825a980ce5557b6c
Tuvshinbayar <tuvshinbayar.dev@gmail.com>
2021-12-21 03:41:44 +0800

    added togle

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,6 +7,11 @@ import LazyLoad from 'react-lazyload';
 import logo from "../assets/logo.png"
 import { Trans, useTranslation } from "react-i18next";
 import i18n from "../i18next";
+import { Fragment, useEffect, useRef, useState } from 'react'
+import {FaAngleDown} from 'react-icons/fa'
+import { Menu, Transition } from '@headlessui/react'
+
+
 
 document.addEventListener('DOMContentLoaded', function() {
     // open
@@ -54,6 +59,229 @@ document.addEventListener('DOMContentLoaded', function() {
 
 const Header = () =>{
     
+
+    function EditInactiveIcon(props) {
+        return (
+          <svg
+            {...props}
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M4 13V16H7L16 7L13 4L4 13Z"
+              fill="#EDE9FE"
+              stroke="#A78BFA"
+              strokeWidth="2"
+            />
+          </svg>
+        )
+      }
+      
+      function EditActiveIcon(props) {
+        return (
+          <svg
+            {...props}
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M4 13V16H7L16 7L13 4L4 13Z"
+              fill="#8B5CF6"
+              stroke="#C4B5FD"
+              strokeWidth="2"
+            />
+          </svg>
+        )
+      }
+
+      function DuplicateInactiveIcon(props) {
+        return (
+          <svg
+            {...props}
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M4 4H12V12H4V4Z"
+              fill="#EDE9FE"
+              stroke="#A78BFA"
+              strokeWidth="2"
+            />
+            <path
+              d="M8 8H16V16H8V8Z"
+              fill="#EDE9FE"
+              stroke="#A78BFA"
+              strokeWidth="2"
+            />
+          </svg>
+        )
+      }
+      
+      function DuplicateActiveIcon(props) {
+        return (
+          <svg
+            {...props}
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M4 4H12V12H4V4Z"
+              fill="#8B5CF6"
+              stroke="#C4B5FD"
+              strokeWidth="2"
+            />
+            <path
+              d="M8 8H16V16H8V8Z"
+              fill="#8B5CF6"
+              stroke="#C4B5FD"
+              strokeWidth="2"
+            />
+          </svg>
+        )
+      }
+      
+      function ArchiveInactiveIcon(props) {
+        return (
+          <svg
+            {...props}
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect
+              x="5"
+              y="8"
+              width="10"
+              height="8"
+              fill="#EDE9FE"
+              stroke="#A78BFA"
+              strokeWidth="2"
+            />
+            <rect
+              x="4"
+              y="4"
+              width="12"
+              height="4"
+              fill="#EDE9FE"
+              stroke="#A78BFA"
+              strokeWidth="2"
+            />
+            <path d="M8 12H12" stroke="#A78BFA" strokeWidth="2" />
+          </svg>
+        )
+      }
+      
+      function ArchiveActiveIcon(props) {
+        return (
+          <svg
+            {...props}
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect
+              x="5"
+              y="8"
+              width="10"
+              height="8"
+              fill="#8B5CF6"
+              stroke="#C4B5FD"
+              strokeWidth="2"
+            />
+            <rect
+              x="4"
+              y="4"
+              width="12"
+              height="4"
+              fill="#8B5CF6"
+              stroke="#C4B5FD"
+              strokeWidth="2"
+            />
+            <path d="M8 12H12" stroke="#A78BFA" strokeWidth="2" />
+          </svg>
+        )
+      }
+      
+      function MoveInactiveIcon(props) {
+        return (
+          <svg
+            {...props}
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M10 4H16V10" stroke="#A78BFA" strokeWidth="2" />
+            <path d="M16 4L8 12" stroke="#A78BFA" strokeWidth="2" />
+            <path d="M8 6H4V16H14V12" stroke="#A78BFA" strokeWidth="2" />
+          </svg>
+        )
+      }
+      
+      function MoveActiveIcon(props) {
+        return (
+          <svg
+            {...props}
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M10 4H16V10" stroke="#C4B5FD" strokeWidth="2" />
+            <path d="M16 4L8 12" stroke="#C4B5FD" strokeWidth="2" />
+            <path d="M8 6H4V16H14V12" stroke="#C4B5FD" strokeWidth="2" />
+          </svg>
+        )
+      }
+      
+      function DeleteInactiveIcon(props) {
+        return (
+          <svg
+            {...props}
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect
+              x="5"
+              y="6"
+              width="10"
+              height="10"
+              fill="#EDE9FE"
+              stroke="#A78BFA"
+              strokeWidth="2"
+            />
+            <path d="M3 6H17" stroke="#A78BFA" strokeWidth="2" />
+            <path d="M8 6V4H12V6" stroke="#A78BFA" strokeWidth="2" />
+          </svg>
+        )
+      }
+      
+      function DeleteActiveIcon(props) {
+        return (
+          <svg
+            {...props}
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect
+              x="5"
+              y="6"
+              width="10"
+              height="10"
+              fill="#8B5CF6"
+              stroke="#C4B5FD"
+              strokeWidth="2"
+            />
+            <path d="M3 6H17" stroke="#C4B5FD" strokeWidth="2" />
+            <path d="M8 6V4H12V6" stroke="#C4B5FD" strokeWidth="2" />
+          </svg>
+        )
+      }
+      
 
     const changeLanguage = (language) => {
         i18n.changeLanguage(language);
@@ -107,14 +335,73 @@ const Header = () =>{
                         
                     </li>
                     <li><a className="text-sm text-gray-400 hover:text-gray-500" target="_blank" href="https://bscscan.com/token/0x2D279FDECdf7f5705F5ff0bD80F8D9a305Ea87F4">BscScan</a></li>
-                    <li className="space-x-5">
-                        <button onClick={() => changeLanguage("mn")}>MN</button>
-                        <button onClick={() => changeLanguage("en")}>EN</button>
-                    </li>
+                   
                 </ul>
-                
-                <a className="hidden xl:inline-block py-2 px-6 bg-blue-500 hover:bg-blue-600 text-sm text-white font-bold rounded-xl transition duration-200" target="_blank" href="whitepaper.pdf">White paper</a>
-            </nav>
+                <div className="w-56">
+                    <Menu as="div" className="relative inline-block text-left">
+                        <div>
+                        <Menu.Button className="inline-flex justify-center items-center w-full px-4 py-2 text-sm font-medium text-white bg-black rounded-md bg-opacity-20 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+                            {t('More')}
+                            <FaAngleDown />
+                        </Menu.Button>
+                        </div>
+                        <Transition
+                        as={Fragment}
+                        enter="transition ease-out duration-100"
+                        enterFrom="transform opacity-0 scale-95"
+                        enterTo="transform opacity-100 scale-100"
+                        leave="transition ease-in duration-75"
+                        leaveFrom="transform opacity-100 scale-100"
+                        leaveTo="transform opacity-0 scale-95"
+                        >
+                        <Menu.Items className="absolute right-0 w-56 mt-2 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                            <div className="px-1 py-1 ">
+                            
+                            </div>
+                            <div className="px-1 py-1">
+                            <Menu.Item onClick={() => changeLanguage("mn")}>
+                                {({ active }) => (
+                                <button
+                                    className={`${
+                                    active ? 'bg-violet-500 text-gray-300' : 'text-gray-900'
+                                    } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
+                                >   
+                                    <button >{t('Mongolia')}</button>
+                                </button>
+                                )}
+                            </Menu.Item>
+                            <Menu.Item onClick={() => changeLanguage("en")}>
+                                {({ active }) => (
+                                <button
+                                    className={`${
+                                    active ? 'bg-violet-500 text-gray-300' : 'text-gray-900'
+                                    } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
+                                >
+                                    <button >{t('English')}</button>
+                                </button>
+                                )}
+                            </Menu.Item>
+                            </div>
+                            <div className="px-1 py-1">
+                            <a href="/whitepaper.pdf" target='_blank'>
+                            <Menu.Item>
+                                {({ active }) => (
+                                <button
+                                    className={`${
+                                    active ? 'bg-violet-500 text-gray-300' : 'text-gray-900'
+                                    } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
+                                >
+                                    WhitePaper
+                                </button>
+                                )}
+                            </Menu.Item>
+                            </a>
+                            </div>
+                        </Menu.Items>
+                        </Transition>
+                    </Menu>
+                </div>
+                </nav>
             <div className="z-50">
                 <Hamburger />
             </div>
