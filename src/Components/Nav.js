8979e69fd028cf89c6cedd55561e9c6479f8c318
Tuvshinbayar <tuvshinbayar.dev@gmail.com>
2021-12-21 19:34:20 +0800

    fixed targets

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -26,31 +26,31 @@ const Nav = () => {
                   <a href="#" className="text-white xl:text-blue-700  rounded" aria-current="page">{t('Home')}</a>
                 </li>
                 <li>
-                  <a href="#" className="text-white">{t('Benefit')}</a>
+                  <a href="#feature" className="text-white">{t('Benefit')}</a>
                 </li>
                 <li>
-                  <a href="#" className="text-white">{t('Services')}</a>
+                  <a href="#service" className="text-white">{t('Services')}</a>
                 </li>
                 <li>
-                  <a href="#" className="text-white">{t('ProjectTeam')}</a>
+                  <a href="#team" className="text-white">{t('ProjectTeam')}</a>
                 </li>
                 <li>
-                  <a href="#" className="text-white">{t('Partners')}</a>
+                  <a href="#company" className="text-white">{t('Partners')}</a>
                 </li>
                 <li>
-                  <a href="#" className="text-white">{t('FAQ')}</a>
+                  <a href="#faq" className="text-white">{t('FAQ')}</a>
                 </li>
                 <li>
-                  <a href="#" className="text-white">{t('ContactUs')}</a>
+                  <a href="#contact" className="text-white">{t('ContactUs')}</a>
                 </li>
                 <li>
-                  <a href="#" className="text-white">{t('BsScan')}</a>
+                  <a href="https://bscscan.com/token/0x2D279FDECdf7f5705F5ff0bD80F8D9a305Ea87F4" target='_blank' className="text-white">{t('BsScan')}</a>
                 </li>
               </ul>
             </div>
             
             <div className='flex items-center justify-center mx-5 space-x-5 text-white'>
-              <a className=' w-24 text-center rounded-lg flex items-center justify-center h-7 bg-blue-500' href='/whitepaper.pdf'>
+              <a className=' w-24 text-center rounded-lg flex items-center justify-center h-7 bg-blue-500' href='/whitepaper.pdf' target='_blank'>
                 WhitePaper
               </a>
               <h1 className=' cursor-pointer' onClick={() => changeLanguage("mn")}>MN</h1>
